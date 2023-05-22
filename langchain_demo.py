3bf216c146665b192accc2dfff28f32a08462048
Vishnu <codemaker2015@gmail.com>
2023-05-22 14:20:53 +0530

    collect user input

--- langchain_demo.py
+++ langchain_demo.py
@@ -8,7 +8,7 @@ API_KEY = env('OPENAI_API_KEY')
 
 # Simple LLM call Using LangChain
 llm = OpenAI(model_name="text-davinci-003", openai_api_key=API_KEY)
-question = "Which language is used to create chatgpt ?"
+question = input("Enter question: ")
 print(question, llm(question))
 
 # Creating a prompt template and running the LLM chain
